dafe796421b3ada7be6ea567748d108eb8cfddb0
w-0123 <Y10001100000@163.com>
2025-06-25 09:28:32 +0800

    Delete aaa/app directory

--- aaa/app/node_modules/react-vant/es/utils/validate/system.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function isAndroid(): boolean;
-export declare function isIOS(): boolean;
--- aaa/app/src/app/admin/wamap/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useEffect, useRef, useState } from "react";
-
-const Amap = () => {
- <div>
-
- </div>
-};
-export default Amap;
